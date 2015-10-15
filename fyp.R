ff407b1f1989bff3eca4c5e8cc84cb2163e23cf3
joelliew91 <joelliew91@gmail.com>
2015-10-15 12:09:43 +0800

    for wk 9

--- fyp.R
+++ fyp.R
@@ -30,11 +30,6 @@ sim_svvg<-function(years){
       return(list(y=y,vol=vol))
 }
 
-est_svvg<-function(iter){
-
-      return(para)
-}
-
 svvg_fn_v<-function(x){
       M = 1/10
       m = 10
@@ -57,35 +52,119 @@ library(MCMCpack)                               ## library required to generate 
 library(HI)                                     ## library required for Adaptive Rejection Metrop Sampling
 set.seed(100)
 delta = 1/250
-para = as.data.frame(cbind(0,0,0.01,0.02,0.5,0,1,0.1,0,1)) ## Choose values randomly but within the domain of posterior
+para = as.data.frame(cbind(0,0.5,0.01,0.01,0.5,0,1,0.1,0,1)) ## Choose values randomly but within the domain of posterior
 names(para) = c("mu","k","theta","sigma.v","rho","gamma","v","sigma","J","G")
 T = 5000
 
 data = sim_svvg(round(T/250))$y
 for(i in 2:round(T/250)){
+      para = rbind(para,rep(0,10))
       ## posterior of v
       v = arms(runif(1,0,1),svvg_fn_v, function(x) (x>0)*(x<1),1)
+      para$v[i] = v ##checked
       ## end of v
-      print("v:")
-      print(v)
+      
+      diff_y = data[i] - data[i-1]
+      diff_v = para$v[i] - para$v[i-1]
+      
       ## posterior of G
       G = arms(runif(1,0,50),svvg_fn_G, function(x) (x>0)*(x<50),1)
+      para$G[i] = G ## checked
       ## end of G
-      print("G:")
-      print(G)
+
       ## posterior of J
       W = 1/((1-para$rho[i-1]^2)*para$v[i-1]*delta)+1/(G*para$sigma[i-1]^2)
-      C = data[i]-data[i-1] - para$mu[i-1]*delta
-      D = v - para$v[i-1] - para$k[i-1]*(para$theta[i-1]-para$v[i-1])*delta
+      C = diff_y - para$mu[i-1]*delta
+      D = diff_v - para$k[i-1]*(para$theta[i-1]-para$v[i-1])*delta
       S = 1/((1-para$rho[i-1]^2)*para$v[i-1]*delta)*(C-para$rho[i-1]*D/para$sigma.v[i-1])+para$gamma[i-1]/para$sigma[i-1]^2
       J = rnorm(1,mean=S/W,sd = 1/sqrt(W))
+      para$J[i] = J ## checked
       ## end of J
       
       ## posterior of sigma
-      
+      m = 2.5
+      M = 5
+      sigma = rinvgamma(1,T/2+m,(1/M+0.5*(sum((para$J[1:i]-para$theta[i-1]*para$G[1:i])^2/para$G[1:i])))^-1)
+      para$sigma[i] = sigma ## checked
       ## end for sigma
-      ## Posterior of N
-      ## End of N
+      
+      ## Posterior of gamma
+      m = 0
+      M = 1
+      W = 1/sigma^2*sum(para$G[1:i])+1/M^2
+      S = 1/sigma^2*sum(para$J[1:i])+m/M^2
+      gam = rnorm(1,mean=S/W,sd = 1/sqrt(W))
+      para$gamma[i] = gam ## checked
+      ## end of gamma
+      
       ## Posterior of mu
+      m = 0
+      M = 1
+      C_n = data[2:i] - data[1:i-1] - delta*para$mu[i-1]
+      D_n = para$v[2:i] - para$v[1:i-1] - para$k[i-1]*(para$theta[i-1]-para$v[1:i-1])*delta
+      W = delta/(1-para$rho[i-1]^2)*sum(1/para$v[1:i-1])+1/M^2
+      S = sum((C_n-para$rho[i-1]*D_n/para$sigma.v[i-1])/para$v[1:i-1])/(1-para$rho[i-1]^2)+m/M^2
+      mu = rnorm(1,mean=S/W,sd = 1/sqrt(W))
+      para$mu[i] = mu
       ## End for mu
+      
+      ## Posterior of Theta
+      m = 0
+      M = 1
+      W = para$k[i-1]^2*delta/(para$sigma.v[i-1]^2*(1-para$rho[i-1]^2))*sum(1/para$v[1:i-1])+1/M^2
+      S = para$k[i-1]/(para$sigma.v[i-1]*(1-para$rho[i-1]^2))*sum((D_n/para$sigma.v[i-1]-para$rho[i-1]*C_n)/para$v[1:i-1])+m/M^2
+      u = runif(1,min=0,max=1)
+      theta = qnorm(u*(1-pnorm(0,mean=S/W,sd = 1/sqrt(W)))+pnorm(0,mean=S/W,sd=1/sqrt(W)),mean=S/W,sd = 1/sqrt(W))
+      para$theta[i] = theta
+      ## end for theta
+      
+      ## Posterior of rho and sigma.v
+      m = 2
+      M = 200
+      C_n_adj = C_n/sqrt(delta*para$v[1:i-1])
+      D_n_adj = D_n/sqrt(delta*para$v[1:i-1])
+      W = sum(C_n_adj^2)+2
+      S = sum(C_n_adj*D_n_adj)
+      w = rinvgamma(1,T/2+m,(0.5*sum(D_n_adj^2)+1/M-S^2/(2*W))^-1)
+      phi = rnorm(1,mean=S/W,sd = sqrt(w/W))
+      sigma.v = w + phi^2
+      rho = phi / sigma.v
+      para$rho[i] = rho
+      para$sigma.v[i] = sigma.v
+      ## end of posterior of rho and sigma.v
+      
+      ## Posterior of k
+      W = delta/((1-para$rho[i-1]^2)*para$sigma.v[i-1]^2)*sum((para$theta[i-1]-para$v[1:i-1])^2/para$v[1:i-1])+1/M^2
+      S = 1/(para$sigma.v[i-1]*(1-para$rho[i-1]^2))*sum(((para$theta[i-1]-para$v[1:i-1])*(D_n/para$sigma.v[i-1]-para$rho[i-1]*C_n))/para$v[1:i-1])
+      u = runif(1,min=0,max=1)
+      print(W)
+      print(S)
+      me = S/W
+      sd = 1/sqrt(W)
+      k = qnorm((1-pnorm(0,mean=me,sd=sd)*u+pnorm(0,mean=me,sd=sd)),mean = me,sd = sd)
+      para$k[i] = k
+      ## end of posterior of k
 }
+
+
+
+
+
+## Posterior of N
+#{A1 = (diff_y - para$mu[i-1]*delta - para$swig[i-1])/sqrt(delta*para$v[i-1])
+#A2 = (diff_v - delta*para$mu[i-1])/sqrt(delta*para$v[i-1])
+#B = (para$v[i] - para$v[i-1] - para$k[i-1]*(para$theta[i-1]-para$v[i-1])*delta)/(para$sigma[i]*sqrt(delta*para$v[i-1]))
+#a1 = exp(-(A1^2-2*para$rho[i-1]*A1*B)/(2*(1-para$rho[i-1]^2)))
+#a2 = exp(-(A2^2 - 2*para$rho[i-1]*A2*B)/(2*(1-para$rho[i-1]^2)))*(1-para$lambda[i-1])
+#N = rbinom(1,1,a1/(a1+a2))
+#para$N = N ## checked
+## End of N
+
+## Posterior of swig
+#C = diff_y - para$mu[i-1]*delta
+#D = diff_v - para$k[i-1]*(para$theta[i-1]-para$v[i-1])*delta
+#S = para$N[i]*(C-para$rho[i-1]*D/para$sigma.v[i-1])/(delta*para$v[i-1]*(1-para$rho[i-1]^2)) + para$mu
+#W =       
+#swig = rnorm(1,mean=S/W,sd = 1/sqrt(W))
+#para$swig[i] = swig
+## end of swig
